16a75e9c04def33512fa8c008882da30e195f0a5
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-03 19:42:27 -0500

    Moved icons into /src since create-react-app doesn't support files beyond that directory.

--- client/src/Helpers/socialMediaIconComponents.js
+++ client/src/Helpers/socialMediaIconComponents.js
@@ -3,21 +3,21 @@ import React from 'react';
 // - Import all social media icon SVGs as functional React Components.
 
 // - Facebook 
-import { ReactComponent as FacebookIconDarkTheme } from '../../public/Icons/SocialMediaIcons/Facebook/FacebookIconDarkTheme.svg';
-import { ReactComponent as FacebookIconLightTheme } from '../../public/Icons/SocialMediaIcons/Facebook/FacebookIconLightTheme.svg';
-import { ReactComponent as FacebookIconHighlighted } from '../../public/Icons/SocialMediaIcons/Facebook/FacebookIconHighlighted.svg';
+import { ReactComponent as FacebookIconDarkTheme } from '../Icons/SocialMediaIcons/Facebook/FacebookIconDarkTheme.svg';
+import { ReactComponent as FacebookIconLightTheme } from '../Icons/SocialMediaIcons/Facebook/FacebookIconLightTheme.svg';
+import { ReactComponent as FacebookIconHighlighted } from '../Icons/SocialMediaIcons/Facebook/FacebookIconHighlighted.svg';
 // - Instagram
-import { ReactComponent as InstagramIconDarkTheme } from '../../public/Icons/SocialMediaIcons/Instagram/InstagramIconDarkTheme.svg';
-import { ReactComponent as InstagramIconLightTheme } from '../../public/Icons/SocialMediaIcons/Instagram/InstagramIconLightTheme.svg';
-import { ReactComponent as InstagramIconHighlighted } from '../../public/Icons/SocialMediaIcons/Instagram/InstagramIconHighlighted.svg';
+import { ReactComponent as InstagramIconDarkTheme } from '../Icons/SocialMediaIcons/Instagram/InstagramIconDarkTheme.svg';
+import { ReactComponent as InstagramIconLightTheme } from '../Icons/SocialMediaIcons/Instagram/InstagramIconLightTheme.svg';
+import { ReactComponent as InstagramIconHighlighted } from '../Icons/SocialMediaIcons/Instagram/InstagramIconHighlighted.svg';
 // - Twitter
-import { ReactComponent as TwitterIconDarkTheme } from '../../public/Icons/SocialMediaIcons/Twitter/TwitterIconDarkTheme.svg';
-import { ReactComponent as TwitterIconLightTheme } from '../../public/Icons/SocialMediaIcons/Twitter/TwitterIconLightTheme.svg';
-import { ReactComponent as TwitterIconHighlighted } from '../../public/Icons/SocialMediaIcons/Twitter/TwitterIconHighlighted.svg';
+import { ReactComponent as TwitterIconDarkTheme } from '../Icons/SocialMediaIcons/Twitter/TwitterIconDarkTheme.svg';
+import { ReactComponent as TwitterIconLightTheme } from '../Icons/SocialMediaIcons/Twitter/TwitterIconLightTheme.svg';
+import { ReactComponent as TwitterIconHighlighted } from '../Icons/SocialMediaIcons/Twitter/TwitterIconHighlighted.svg';
 // - Spotify
-import { ReactComponent as SpotifyIconDarkTheme } from '../../public/Icons/SocialMediaIcons/Spotify/SpotifyIconDarkTheme.svg';
-import { ReactComponent as SpotifyIconLightTheme } from '../../public/Icons/SocialMediaIcons/Spotify/SpotifyIconLightTheme.svg';
-import { ReactComponent as SpotifyIconHighlighted } from '../../public/Icons/SocialMediaIcons/Spotify/SpotifyIconHighlighted.svg';
+import { ReactComponent as SpotifyIconDarkTheme } from '../Icons/SocialMediaIcons/Spotify/SpotifyIconDarkTheme.svg';
+import { ReactComponent as SpotifyIconLightTheme } from '../Icons/SocialMediaIcons/Spotify/SpotifyIconLightTheme.svg';
+import { ReactComponent as SpotifyIconHighlighted } from '../Icons/SocialMediaIcons/Spotify/SpotifyIconHighlighted.svg';
 // - Bandcamp
 //import { ReactComponent as }
 
