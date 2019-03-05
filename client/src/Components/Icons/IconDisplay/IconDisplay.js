f26887b90e9b5167461f79f5e56af0aefba6e09a
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-05 16:03:50 -0500

    Implemented an IconDisplay component to display icons that aren't buttons (like logos for example)

---
+++ client/src/Components/Icons/IconDisplay/IconDisplay.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ClassNames from 'classnames';
+import PropTypes from 'prop-types';
+import { ThemeContext } from '../../../ThemeContext';
+
+import styles from './IconDisplay.module.scss';
+
+class IconDisplay extends React.Component {
+
+	static propTypes = {
+		size: PropTypes.string
+	};
+
+	static defaultProps = {
+		size: '30px'
+	};
+
+	render() {
+
+		let icon;
+		if (this.context === 'dark') icon = this.props.darkTheme;
+		else icon = this.props.lightTheme;
+
+		const themeClass = (this.context === 'dark') ? styles.darkTheme : styles.lightTheme;
+		const displayClasses = ClassNames(styles.display, themeClass);
+
+		const style = {
+			width: this.props.size,
+			height: this.props.size
+		};
+
+		return (
+			<div 
+				className={ displayClasses }
+				style={ style }
+			>
+				
+			</div>
+		);
+	}
+}
+
+IconDisplay.contextType = ThemeContext;
+
+export default IconDisplay;
