84d41e513434293cb286b3499c0ce0ce77c2417c
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-10 16:27:36 -0400

    Working on SideBar. Going to make CommunityLinks its own component and store it locally within the SideBar component's folder structure.

--- client/src/Components/SideMenu/SideMenu.js
+++ client/src/Components/SideMenu/SideMenu.js
@@ -47,9 +47,101 @@ class SideMenu extends React.Component {
 				<div className={ styles.hDividerCradle }>
 					<HorizontalDivider height='3px'/>
 				</div>
+			  {/* These will have to be <Link>s when routing is set up. */}
+				<div className={ styles.buttonGrid }>
+					<OutlineButton
+						text='Home'
+						shape='rounded'
+						onClick={ () => {} }
+					/>	
+					<OutlineButton
+						text='About'
+						shape='rounded'
+						onClick={ () => {} }
+					/>	
+					<OutlineButton
+						text='Documentation'
+						shape='rounded'
+						onClick={ () => {} }
+					/>	
+				</div>
 				<h3 className={ themeClass }>Community Links</h3>
 				<div className={ styles.hDividerCradle }>
 					<HorizontalDivider height='3px'/>
+				</div>
+				<div className={ styles.communityLinks }>
+					<div className={ styles.linkItem }>
+						<div className={ styles.button }>
+							<a
+								href='https://www.facebook.com/groups/wav.city/'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<OutlineButton
+									text='Wave City'
+									shape='rounded'
+									onClick={ () => {} }
+								/>
+							</a>
+						</div>
+						<div className={ styles.label }>
+							<h4 className={ themeClass }>Facebook Group</h4>
+						</div>
+					</div>
+					<div className={ styles.linkItem }>
+						<div className={ styles.button }>
+							<a
+								href='https://www.reddit.com/r/wavepool'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<OutlineButton
+									text='/r/wavepool'
+									shape='rounded'
+									onClick={ () => {} }
+								/>
+							</a>
+						</div>
+						<div className={ styles.label }>
+							<h4 className={ themeClass }>Reddit Group</h4>
+						</div>
+					</div>					
+					<div className={ styles.linkItem }>
+						<div className={ styles.button }>
+							<a
+								href='https://discordapp.com/invite/YFNVcyG'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<OutlineButton
+									text='Wavepool'
+									shape='rounded'
+									onClick={ () => {} }
+								/>
+							</a>
+						</div>
+						<div className={ styles.label }>
+							<h4 className={ themeClass }>Discord Server</h4>
+						</div>
+					</div>
+					<div className={ styles.linkItem }>
+						<div className={ styles.button }>
+							<a
+								href='https://discordapp.com/invite/theaccidentalpoet'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<OutlineButton
+									text='The Accidental Poet'
+									shape='rounded'
+									onClick={ () => {} }
+								/>
+							</a>
+						</div>
+						<div className={ styles.label }>
+							<h4 className={ themeClass }>Discord Server</h4>
+						</div>
+					</div>					
 				</div>
 			</div>
 		);
