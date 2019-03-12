be8f29db2fa81dbf3bb3ef1465af2dba7557fa55
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-12 01:04:11 -0400

    Added all artists from Neo4J database and populated in their links.

--- client/src/Datasets/artists.js
+++ client/src/Datasets/artists.js
@@ -50,7 +50,7 @@ const artists = [
   }, 
   {
     name: 'Juche',
-    imageURL: process.env.PUBLIC_URL + 'Images/Juche.jpg',
+    imageURL: process.env.PUBLIC_URL + '/Images/Juche.jpg',
     socialURLs: {
       spotify: 'https://open.spotify.com/artist/3GZ8Rfap7VxAOzABiZEXCL',
       soundCloud: 'https://soundcloud.com/erejuche',
@@ -63,7 +63,7 @@ const artists = [
     }
   },
   {
-    name: 'Mingo',
+    name: 'M!NGO',
     imageURL: process.env.PUBLIC_URL + '/Images/Mingo.jpg',
     socialURLs: {
       spotify: 'https://open.spotify.com/artist/0atqNWuCKAyb6BMWuEByhE',
@@ -75,99 +75,395 @@ const artists = [
     }
   }, 
   // {
-  //   name: '',
-  //   imageURL: ,
+  //   name: 'brothel',
+  //   imageURL: process.env.PUBLIC_URL + '/Images/Brothel.jpg',
   //   socialURLs: {
-  //     spotify: '',
-  //     soundCloud: '',
-  //     apple: '',
-  //     bandcamp: '',
-  //     instagram: '',
-  //     facebook: '',
-  //     twitter: '',
-  //     youtube: '',
-  //     vk: '',
-  //     personalWebSite: ''
+  //     spotify: 'https://open.spotify.com/artist/3xYvvJ6tjXyJJdEXBs8qf0',
+  //     soundCloud: 'https://soundcloud.com/brothelmusic',
+  //     bandcamp: 'https://brothelmusic.bandcamp.com/',
+  //     instagram: 'https://www.instagram.com/brothel.brothel',
+  //     facebook: 'https://www.facebook.com/brothelofficial/',
+  //     twitter: 'https://twitter.com/brothelmusic',
+  //     personalWebSite: 'https://www.brothelmerch.com/'
   //   }
   // }, 
+  {
+    name: 'Deadcrow',
+    imageURL: process.env.PUBLIC_URL + '/Images/Deadcrow.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/4ufkY8hmhmYl4aCnzv3dLE',
+      soundCloud: 'https://soundcloud.com/deadcrxw',
+      bandcamp: 'https://deadcrow.bandcamp.com/',
+      instagram: 'https://www.instagram.com/devdcrow/',
+      facebook: 'https://www.facebook.com/deadcrxw/',
+      twitter: 'https://twitter.com/devdcrow'
+    }
+  },
+  {
+    name: 'w/Out',
+    imageURL: process.env.PUBLIC_URL + '/Images/wOut.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/5gtHKg86NBBz20yUOO8f7b?si=BndP6BmgRTyWh5SEZFjAxA',
+      soundCloud: 'https://soundcloud.com/thisiswithout',
+      instagram: 'https://www.instagram.com/thisiswithout/',
+      facebook: 'https://www.facebook.com/thisiswithout/',
+      twitter: 'https://twitter.com/thisiswithout'
+    }
+  },
+  // {
+  //   name: 'INFDL',
+  //   imageURL: process.env.PUBLIC_URL + '/Images/INFDL.jpg',
+  //   socialURLs: {
+  //     spotify: 'https://open.spotify.com/artist/4DFdFV95TzpGUCKw9ZFI9M?si=Coow2mcWQ2mztZrxVwrZvA',
+  //     soundCloud: 'https://soundcloud.com/infdl',
+  //     instagram: 'https://www.instagram.com/infdlmusic/',
+  //     facebook: 'https://www.facebook.com/Infdlmusic/',
+  //     twitter: 'https://twitter.com/INFDLMusic',
+  //     personalWebSite: 'https://www.infdlmusic.com/'
+  //   }
+  // }, 
+  {
+    name: 'Othos',
+    imageURL: process.env.PUBLIC_URL + '/Images/Othos.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/4FpzcfvOp8S5vJKTJJ24wo',
+      soundCloud: 'https://soundcloud.com/othosofficial',
+      instagram: 'https://www.instagram.com/othos.music/',
+      facebook: 'https://www.facebook.com/othosofficial',
+      twitter: 'https://twitter.com/Othosmusic',
+      youtube: 'https://www.youtube.com/channel/UCklc3pcPPGQTS-S14a6Yt_A'
+    }
+  },
+  {
+    name: 'Giant Ibis',
+    imageURL: process.env.PUBLIC_URL + '/Images/GiantIbis.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/5nMsZgYVD86BSKYjhQKP5A',
+      soundCloud: 'https://soundcloud.com/giantibismusic',
+      bandcamp: 'https://giantibis.bandcamp.com/',
+      instagram: 'https://www.instagram.com/giantibis/',
+      twitter: 'https://twitter.com/Giant_ibis'
+    }
+  }, 
+  {
+    name: 'lostageless',
+    imageURL: process.env.PUBLIC_URL + '/Images/LostAgeless.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/5X7LQl3tw4RQ26KxesuX4g',
+      soundCloud: 'https://soundcloud.com/lostageless',
+      apple: 'https://itunes.apple.com/us/artist/lostageless/1412725957',
+      bandcamp: 'https://lostageless.bandcamp.com/',
+      instagram: 'https://www.instagram.com/lostageless/',
+      facebook: 'https://www.facebook.com/lostageless/'
+    }
+  },
+  {
+    name: 'Sleep Well',
+    imageURL: process.env.PUBLIC_URL + '/Images/SleepWell.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/6m1l2bhuOoVgQ8CRQAy8tb',
+      soundCloud: 'https://soundcloud.com/feelingsleepy',
+      instagram: 'https://www.instagram.com/phvntm/',
+      twitter: 'https://twitter.com/phvntm'
+    }
+  },
+  {
+    name: 'Resonata',
+    imageURL: process.env.PUBLIC_URL + '/Images/Resonata.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/0DPxk01Zpe39U3H7gTBj4H',
+      soundCloud: 'https://soundcloud.com/resonata',
+      instagram: 'https://www.instagram.com/resonata_/',
+      facebook: 'https://www.facebook.com/ResonataOfficial/',
+      twitter: 'https://twitter.com/resonata_'
+    }
+  }, 
+  {
+    name: 'Kareful',
+    imageURL: process.env.PUBLIC_URL + '/Images/Kareful.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/7y4breKexfBWgdyMgHMEHK',
+      soundCloud: 'https://soundcloud.com/karefuluk',
+      apple: 'https://itunes.apple.com/gb/artist/kareful/673916892',
+      bandcamp: 'https://kareful.bandcamp.com/releases',
+      instagram: 'https://www.instagram.com/neverkareful/',
+      facebook: 'https://www.facebook.com/karefulofficial/?notif_t=page_fan',
+      twitter: 'https://twitter.com/KarefulUK'
+    }
+  }, 
+  {
+    name: 'TW1GZ',
+    imageURL: process.env.PUBLIC_URL + '/Images/TW1GZ.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/4ulN3hJ5lQ15oqHDLxVNx2',
+      soundCloud: 'https://soundcloud.com/tw1gz',
+      bandcamp: 'https://tw1gz.bandcamp.com/',
+      instagram: 'https://www.instagram.com/tw1gz/',
+      facebook: 'https://www.facebook.com/TW1GZMUSIC/',
+      twitter: 'https://twitter.com/_tw1gz',
+      youtube: 'https://www.youtube.com/channel/UC8UzhAq-pYYSHtHFOUwdv0w'
+    }
+  }, 
+  {
+    name: 'Djedi',
+    imageURL: process.env.PUBLIC_URL + '/Images/Djedi.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/59eedBe7vYyyWDyqHZ0IYd',
+      soundCloud: 'https://soundcloud.com/djedimusic',
+      bandcamp: 'https://djedi.bandcamp.com/',
+      instagram: 'https://www.instagram.com/brett_djedi/',
+      facebook: 'https://www.facebook.com/djedimusic',
+      twitter: 'https://twitter.com/brett_djedi'
+    }
+  }, 
+  {
+    name: 'Medici',
+    imageURL: process.env.PUBLIC_URL + '/Images/Medici.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/06v75CwBsO96RLOoZbyXgD',
+      soundCloud: 'https://soundcloud.com/medicisounds',
+      bandcamp: 'https://medicisounds.bandcamp.com/',
+      instagram: 'https://www.instagram.com/medicisounds/',
+      facebook: 'https://www.facebook.com/medicisounds/',
+      youtube: 'https://www.youtube.com/channel/UC5i-2AOBumlXtT027PFhhYw'
+    }
+  },
+  {
+    name: 'MRKRYL',
+    imageURL: process.env.PUBLIC_URL + '/Images/MRKRYL.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/5AJeziFEwkw9lvA4HPXcNt',
+      soundCloud: 'https://soundcloud.com/mrkryl ',
+      bandcamp: 'https://mrkrylofficial.bandcamp.com/releases',
+      instagram: 'https://www.instagram.com/mrkryl/',
+      facebook: 'https://www.facebook.com/sometimessomethingiseverything/',
+      twitter: 'https://twitter.com/MRKRYL',
+      youtube: 'https://www.youtube.com/channel/UCt4EImIF34Vkze9L-SnEJfQ'
+    }
+  }, 
+  {
+    name: 'Skit',
+    imageURL: process.env.PUBLIC_URL + '/Images/Skit.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/6ejpWdV51Wxr3azIpJXwoQ?lf=',
+      soundCloud: 'https://soundcloud.com/skit',
+      instagram: 'https://www.instagram.com/skitofficial/',
+      twitter: 'https://twitter.com/skitofficial'
+    }
+  },
+  {
+    name: 'Ludicy',
+    imageURL: process.env.PUBLIC_URL + '/Images/Ludicy.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/7BdHyuZ5YS6YwecHSeHiXu',
+      soundCloud: 'https://soundcloud.com/ludicysounds',
+      bandcamp: 'https://ludicysounds.bandcamp.com/releases',
+      instagram: 'https://www.instagram.com/sound_weaver/',
+      facebook: 'https://www.facebook.com/ludicysoundsofficial',
+      twitter: 'https://twitter.com/ludicysounds'
+    }
+  },
+  {
+    name: 'Plastician',
+    imageURL: process.env.PUBLIC_URL + '/Images/Plastician.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/6p41GgJajkf3W2YXAzL8xC',
+      soundCloud: 'https://soundcloud.com/plastician',
+      bandcamp: 'https://plastician.bandcamp.com/',
+      instagram: 'https://www.instagram.com/plastician/',
+      facebook: 'https://www.facebook.com/plastician',
+      twitter: 'https://twitter.com/plastician'
+    }
+  },
+  {
+    name: 'ONHELL',
+    imageURL: process.env.PUBLIC_URL + '/Images/Onhell.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/66SJhirDGiBwr3tfvu3a0t',
+      soundCloud: 'https://soundcloud.com/onhellmusic',
+      bandcamp: 'https://onhell.bandcamp.com/',
+      instagram: 'https://www.instagram.com/onhellmusic/',
+      facebook: 'https://www.facebook.com/home.php#!/pages/DJ-OnHell/162299850498449',
+      twitter: 'https://twitter.com/onhellmusic',
+      youtube: 'https://www.youtube.com/user/onhellmusic'
+    }
+  },
+  {
+    name: 'AL_NE',
+    imageURL: process.env.PUBLIC_URL + '/Images/AL_NE.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/7AHnoNiNhFM87Y3Rhzw1HU',
+      soundCloud: 'https://soundcloud.com/al_ne',
+      bandcamp: 'https://alonealex.bandcamp.com/',
+      instagram: 'https://www.instagram.com/alexmezatis/',
+      twitter: 'https://twitter.com/AL_NEmusic'
+    }
+  },
+  {
+    name: 'Shxde',
+    imageURL: process.env.PUBLIC_URL + '/Images/Shxde.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/6rCnAlUYZj7i19NEeRlwCr',
+      soundCloud: 'https://soundcloud.com/shxde',
+      apple: 'https://itunes.apple.com/us/artist/shxde/1268489866',
+      bandcamp: 'https://shxde.bandcamp.com/',
+      instagram: 'https://www.instagram.com/shxde.wav/',
+      twitter: 'https://twitter.com/soundofshxde',
+      youtube: 'https://www.youtube.com/channel/UCNXEIKsTvdd8Fif2DPVwzag'
+    }
+  }, 
+  {
+    name: 'LTHL',
+    imageURL: process.env.PUBLIC_URL + '/Images/LTHL.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/00DUVKhfHjyNKmN9xBZHOJ',
+      soundCloud: 'https://soundcloud.com/lthl',
+      bandcamp: 'https://lthl.bandcamp.com/',
+      instagram: 'https://www.instagram.com/lthlgrm/',
+      facebook: 'https://www.facebook.com/lthlgrm',
+      twitter: 'https://twitter.com/LTHLgrm'
+    }
+  }, 
+  // {
+  //   name: 'Martian Moon',
+  //   imageURL: process.env.PUBLIC_URL + '/Images/MartianMoon.png',
+  //   socialURLs: {
+  //     spotify: 'https://open.spotify.com/artist/58zBD9v1ac86uikZ90oJVJ',
+  //     soundCloud: 'https://soundcloud.com/martianmoon/',
+  //     apple: 'https://itunes.apple.com/gr/artist/martian-moon/1262141379?app=music&ign-mpt=uo%3D4',
+  //     bandcamp: 'https://martianmoon.bandcamp.com/',
+  //     instagram: 'https://www.instagram.com/martianmoonmusic/ ',
+  //     facebook: 'https://www.facebook.com/MartianMoonMusic/',
+  //     twitter: 'https://twitter.com/MartianMoonCA',
+  //     personalWebSite: 'https://www.martianmoon.ca/'
+  //   }
+  // }, 
+  {
+    name: 'pinknokia',
+    imageURL: process.env.PUBLIC_URL + '/Images/PinkNokia.png',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/1wvxUyR1z0TFoLz9uOsEGO',
+      soundCloud: 'https://soundcloud.com/pinknokia',
+      instagram: 'https://www.instagram.com/joshbroe/'
+    }
+  }, 
+  {
+    name: 'Stohou',
+    imageURL: process.env.PUBLIC_URL + '/Images/Stohou.jpg',
+    socialURLs: {
+      spotify: 'https://open.spotify.com/artist/5uvliUUGC52KUZPTrZkedA',
+      soundCloud: 'https://soundcloud.com/stohou',
+      instagram: 'https://www.instagram.com/stohou/',
+      facebook: 'https://www.facebook.com/stohou/',
+      twitter: 'https://twitter.com/stohoumusic'
+    }
+  },
+  // {
+  //   name: 'Hoodie',
+  //   imageURL: process.env.PUBLIC_URL + '/Images/Hoodie.jpg',
+  //   socialURLs: {
+  //     spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=43MH15YTRJKoNMOmm7SIkA',
+  //     soundCloud: 'https://soundcloud.com/hoodiegen',
+  //     apple: 'https://itunes.apple.com/us/artist/hoodie/1286330076',
+  //     bandcamp: 'https://hoodie1.bandcamp.com/',
+  //     instagram: 'https://www.instagram.com/hoodiegen/',
+  //     twitter: 'https://www.twitter.com/hoodiegen',
+  //     personalWebSite: 'https://hoodiegen.com'
+  //   }
+  // },  
   {
     name: 'Cashforgold',
     imageURL: process.env.PUBLIC_URL + '/Images/Cashforgold.jpg',
     socialURLs: {
-      soundCloud: 'https://soundcloud.com/hoodiegen',
-      instagram: 'https://instagram.com/hoodiegen',
-      twitter: 'https://twitter.com/hoodiegen',
-      spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=RTo4wyZ8Q_uds2jyJbIvCA',
-      personalWebsite: 'https://hoodiegen.com',
-      //bandCamp: 'https://hoodie1.bandcamp.com/releases'
-      // appleMusic: 'https://itunes.apple.com/us/artist/hoodie/1286330076'  
+      spotify: 'https://open.spotify.com/artist/4FzthA7DjutA71z91I1DKX',
+      soundCloud: 'https://soundcloud.com/cashforgoldd',
+      bandcamp: 'https://cashforgoldmusic.bandcamp.com/',
+      instagram: 'https://www.instagram.com/cash.forgold/',
+      youtube: 'https://www.youtube.com/channel/UC6RhKqVcJhXAsVzVXzgNILA',
+      twitter: 'https://twitter.com/cashforgold___',
+      personalWebsite: 'https://www.cashforgoldmusic.com/'
     }  
   },
   {
     name: 'Arael',
     imageURL: process.env.PUBLIC_URL + '/Images/Arael.png',
     socialURLs: {
-      soundCloud: 'https://soundcloud.com/hoodiegen',
-      instagram: 'https://instagram.com/hoodiegen',
-      twitter: 'https://twitter.com/hoodiegen',
-      spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=RTo4wyZ8Q_uds2jyJbIvCA',
-      personalWebsite: 'https://hoodiegen.com',
-      //bandCamp: 'https://hoodie1.bandcamp.com/releases'
-      // appleMusic: 'https://itunes.apple.com/us/artist/hoodie/1286330076'     
+      spotify: 'https://open.spotify.com/artist/7KjOBaQSexjUgj0Z0YrKzO',
+      soundCloud: 'https://soundcloud.com/araelsounds',
+      bandcamp: 'https://araelsounds.bandcamp.com/',
+      instagram: 'https://www.instagram.com/araelsounds/',
+      twitter: 'https://twitter.com/araelsounds'
     }
   },
   {
     name: 'Barnacle Boi',
     imageURL: process.env.PUBLIC_URL + '/Images/BarnacleBoi.jpg',
     socialURLs: {
-      soundCloud: 'https://soundcloud.com/hoodiegen',
-      instagram: 'https://instagram.com/hoodiegen',
-      twitter: 'https://twitter.com/hoodiegen',
-      spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=RTo4wyZ8Q_uds2jyJbIvCA',
-      personalWebsite: 'https://hoodiegen.com',
-      //bandCamp: 'https://hoodie1.bandcamp.com/releases'
-      // appleMusic: 'https://itunes.apple.com/us/artist/hoodie/1286330076'     
+      spotify: 'https://open.spotify.com/artist/6psDObCnL4BfpwqYzE0Dzu',
+      soundCloud: 'https://soundcloud.com/barnacleboimusic',
+      apple: 'https://itunes.apple.com/us/artist/barnacle-boi/268706623',
+      bandcamp: 'https://barnacleboi.bandcamp.com/',
+      youtube: 'https://www.youtube.com/c/barnacleboi',
+      instagram: 'https://www.instagram.com/barnacleboiofficial/',
+      facebook: 'https://www.facebook.com/barnacleboimusic',
+      twitter: 'https://twitter.com/barnacleboi777',
+      vk: 'https://vk.com/id438791682',
+      personalWebsite: 'https://barnacleboi.com/'
     }   
   },
   {
     name: 'Kavern',
     imageURL: process.env.PUBLIC_URL + '/Images/Kavern.jpg',
     socialURLs: {
-      soundCloud: 'https://soundcloud.com/hoodiegen',
-      instagram: 'https://instagram.com/hoodiegen',
-      twitter: 'https://twitter.com/hoodiegen',
-      spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=RTo4wyZ8Q_uds2jyJbIvCA',
-      personalWebsite: 'https://hoodiegen.com',
-      //bandCamp: 'https://hoodie1.bandcamp.com/releases'
-      // appleMusic: 'https://itunes.apple.com/us/artist/hoodie/1286330076'     
-    }    
+      spotify: 'https://open.spotify.com/artist/1XEc0aycEzmxYuZfpY5Q7e',
+      soundCloud: 'https://soundcloud.com/kavern-beats',
+      bandcamp: 'https://kavern.bandcamp.com/',
+      instagram: 'https://www.instagram.com/kavernbeats/',
+      facebook: 'https://www.facebook.com/Kavernbeats/',
+      twitter: 'https://twitter.com/KavernBeats'
+    }
   },
   {
     name: 'Kyddiekafka',
     imageURL: process.env.PUBLIC_URL + '/Images/Kyddiekafka.jpg',
     socialURLs: {
-      soundCloud: 'https://soundcloud.com/hoodiegen',
-      instagram: 'https://instagram.com/hoodiegen',
-      twitter: 'https://twitter.com/hoodiegen',
-      spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=RTo4wyZ8Q_uds2jyJbIvCA',
-      personalWebsite: 'https://hoodiegen.com',
-      //bandCamp: 'https://hoodie1.bandcamp.com/releases'
-      // appleMusic: 'https://itunes.apple.com/us/artist/hoodie/1286330076'     
+      spotify: 'https://open.spotify.com/artist/2WEqx25N8Xb7dHMjRmaalG?si=hRxn2th2QaWkGRwOh609WA',
+      soundCloud: 'https://soundcloud.com/kyddiekafka',
+      bandcamp: 'https://kyddiekafka.bandcamp.com/',
+      youtube: 'https://www.youtube.com/user/SilvestreEditions',
+      instagram: 'https://www.instagram.com/kyddiekafka/',
+      facebook: 'https://www.facebook.com/kyddiekafkamusic',
+      twitter: 'https://twitter.com/kyddiekafka'
     }    
   },
   {
     name: 'Sidewalks and Skeletons',
     imageURL: process.env.PUBLIC_URL + '/Images/SidewalksAndSkeletons.png',
     socialURLs: {
-      soundCloud: 'https://soundcloud.com/hoodiegen',
-      instagram: 'https://instagram.com/hoodiegen',
-      twitter: 'https://twitter.com/hoodiegen',
-      spotify: 'https://open.spotify.com/artist/08kgcpEQscDxkhVLXRkGXK?si=RTo4wyZ8Q_uds2jyJbIvCA',
-      personalWebsite: 'https://hoodiegen.com',
-      //bandCamp: 'https://hoodie1.bandcamp.com/releases'
-      // appleMusic: 'https://itunes.apple.com/us/artist/hoodie/1286330076'     
+      spotify: 'https://open.spotify.com/artist/48nHO1cuTbpx4ELhChsxX1',
+      soundCloud: 'https://soundcloud.com/jakeleeismyname',
+      apple: 'https://itunes.apple.com/gb/artist/sidewalks-and-skeletons/id678419002',
+      bandcamp: 'https://sidewalksandskeletons.bandcamp.com/',
+      youtube: 'https://www.youtube.com/c/SidewalksandSkeletons',
+      instagram: 'https://www.instagram.com/sidewalksandskeletons/',
+      facebook: 'https://www.facebook.com/sidewalksandskeletons/',
+      twitter: 'https://twitter.com/JakeLeeeee'
     }     
-  }  
+  },
+  // {
+  //   name: '',
+  //   imageURL: process.env.PUBLIC_URL + '/Images/',
+  //   socialURLs: {
+  //     spotify: '',
+  //     soundCloud: '',
+  //     apple: '',
+  //     bandcamp: '',
+  //     youtube: '',
+  //     instagram: '',
+  //     facebook: '',
+  //     twitter: ''
+  //   }     
+  // },
 ];
 
 export default artists;
