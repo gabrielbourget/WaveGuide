eb84a594c85ea921719c154a9f7d6c2034245d00
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-15 14:14:40 -0400

    Cleaned up App component, staged it for setting up routes. Proceeding with HomePage component dev now.

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './App.module.css';
 import { ThemeContext } from '../../ThemeContext';
 
-// import artists from '../../Datasets/artists';
-
 // import ColumnWithPaddingPageCradle 
 //   from '../Cradles/ColumnWithPaddingPageCradle/ColumnWithPaddingPageCradle';
 // import MiddleContentCradle from '../Cradles/MiddleContentCradle/MiddleContentCradle';
@@ -22,80 +20,48 @@ import { sortCriteriaEnum } from '../../Helpers/generalDataStructures'
 
 class App extends React.Component {
 
-  // - TODO -> setState call should be a store dispatch when redux is set up.
-  //           Call props function passed down from container component, which
-  //           will get reducer to manipulate state.
-  backdropClickHandler = () => {
-    this.setState({ 
-      sideMenuOpen: false,
-      backdropOpen: false
-    });
-  };
-
   // - Dispatch initial API call to the Neo4J database here
   componentWillMount() {
 
-    // - Probably dispatch this out as an action eventually once Redux is hooked up.
     const clientDateTime = new Date();
     const hour = clientDateTime.getHours();
-    // - OLD -> (hour < 12) ? this.setState({ theme:'dark' }) : this.setState({ theme:'light'});
     (hour < 12) ? this.props.switchTheme('dark') : this.props.switchTheme('light');
-    
-    // - Use this once redux is active. This will mock requesting initial data from server. 
+
+    // - Right now this is a call to a local javascript object. Eventually, that 
+    //   dispatch function will make an asynchronous request to a remote Neo4J database. 
     this.props.loadArtists();
   };
 
   render(){
-
-    let backdrop;
-    //console.log('backdrop props');
-    //console.log(this.props.settings.backdropOpen);
-    //if (this.props.settings.backdropOpen){ //once redux is set up.
-    // - OLD -> if (this.state.sideMenuOpen) {
-      //backdrop = <Backdrop onClick={ this.props.toggleBackdrop }/>
-    //}
-
     return (
-      // - Draw this from mapped props once redux is set up.
-      // OLD -> value={ this.state.theme }
       <ThemeContext.Provider value={ this.props.settings.theme }> 
         <div className={ styles.app }>
           <Navbar 
-            // - Active once redux is running.
             searchThroughArtists={ this.props.searchThroughArtists }
             sideMenuOpen={ this.props.settings.sideMenuOpen }
-            // OLD -> onSideMenuButtonClick={ this.toggleSideMenu }
             onSideMenuButtonClick={ this.props.toggleSideMenu }
-          />        
-          {/*<FromTheTopCradle>*/}
-            <ArtistProfileDisplay 
-              displayMode='gallery'
-              onSortClick={ this.sortArtists }
-              // - Active once redux is running.
-              onSortAlphabeticalClick={ this.props.sortArtistsAlphabetical }
-              onSortRevAlphabeticalClick={ this.props.sortArtistsReverseAlphabetical }
-              
-              // - OLD -> artists={ this.state.artists }
-              // artists={ this.props.artists }
-              artists={ this.props.displayedArtists }
-            />    
-          {/*</FromTheTopCradle>*/}
+          /> 
+          {/* TODO -> HOME PAGE ROUTE/COMPONENT GOES HERE */}
+          {/* TODO -> RENDER INTO A ROUTE EVENTUALLY */}
+          <ArtistProfileDisplay 
+            displayMode='gallery'
+            onSortClick={ this.sortArtists }
+            onSortAlphabeticalClick={ this.props.sortArtistsAlphabetical }
+            onSortRevAlphabeticalClick={ this.props.sortArtistsReverseAlphabetical }
+            artists={ this.props.displayedArtists }
+          />   
+          {/* TODO -> DOCUMENTATION ROUTE/COMPONENT GOES HERE */}
           <Footer/>
-          {/* SideMenu goes here */}
           <SideMenu 
-            // - OLD -> currTheme={ this.state.theme }
             currTheme={ this.props.settings.theme }
-            // - OLD -> open={ this.state.sideMenuOpen } 
             open={ this.props.settings.sideMenuOpen }
-            // OLD -> onThemeSwitch={ this.switchTheme }
             onThemeSwitch={ this.props.switchTheme }
           />
           { backdrop }
         </div> 
-      </ThemeContext.Provider>
-     
-    ); // return
-  }; // render()
+      </ThemeContext.Provider>    
+    ); // - return
+  }; // - render()
 }
 
 export default App;
