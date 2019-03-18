426f33b9eb10d5ed4b00c03e2a2c0a80fb01a6df
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-17 20:26:09 -0400

    Worked on outlining documentation section

---
+++ client/src/Components/LargeScopeComponents/DocumentationDisplay/DocumentationTree/DocumentationTree.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeContext } from '../../../../ThemeContext';
+
+import styles from './styles';
+import { prepareComponent } from './helpers';
+
+class DocumentationTree extends React.Component {
+	render() {
+		return (
+
+		);
+	}
+}
+
+DocumentationTree.contextType = ThemeContext;
+
+export default DocumentationTree;
---
+++ client/src/Components/LargeScopeComponents/DocumentationDisplay/DocumentationTree/helpers.js
@@ -0,0 +1,12 @@
+import ClassNames from 'classnames';
+
+export const prepareComponent( context, props, styles ) => {
+		
+	const themeClass = (context === 'dark') ? styles.darkTheme : styles.lightTheme;
+
+	const documentationTreeClasses = ClassNames(styles.documentationTree, themeClass);
+
+	return {
+		documentationTreeClasses
+	};
+};
