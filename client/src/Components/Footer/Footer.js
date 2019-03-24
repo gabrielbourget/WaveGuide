9e7d0c8015b919fd0689d372a52042234b5db12a
Gabriel Bourget <gabrielbourget@gmail.com>
2019-03-24 03:12:43 -0400

    Did some work on the footer.

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ClassNames from 'classnames';
 import { ThemeContext } from '../../ThemeContext';
 
-import styles from './Footer.module.scss';
-import WaveIconDarkTheme from './SVG/WaveIcon/WaveIconDarkTheme';
-//import { ReactComponent as WaveIconDarkTheme } from './SVG/WaveIcon/WaveIconDarkTheme.svg';
-import { ReactComponent as  WaveIconLightTheme } from './SVG/WaveIcon/WaveIconLightTheme.svg';
-
-import IconDisplay from '../Icons/IconDisplay/IconDisplay';
+// import IconDisplay from '../Icons/IconDisplay/IconDisplay';
 import Paragraph from '../TextLayout/Paragraph/Paragraph';
 import OutlineButton from '../Buttons/OutlineButton/OutlineButton';
+
+import styles from './Footer.module.scss';
+// import WaveIconDarkTheme from './SVG/WaveIcon/WaveIconDarkTheme';
+//import { ReactComponent as WaveIconDarkTheme } from './SVG/WaveIcon/WaveIconDarkTheme.svg';
+// import { ReactComponent as  WaveIconLightTheme } from './SVG/WaveIcon/WaveIconLightTheme.svg';
+
+import waveImageDarkTheme from './SVG/WaveIcon/WaveIconImageDarkTheme.jpg';
+import waveImageLightTheme from './SVG/WaveIcon/WaveIconImageLightTheme.jpg';
 
 class Footer extends React.Component {
 	render() {
@@ -20,51 +23,53 @@ class Footer extends React.Component {
 		return (
 			<div className={ footerClasses }>
 				<div className={ styles.logo }>
-					<IconDisplay
+{/*					<IconDisplay
 						size='75px'
 						darkTheme={ <WaveIconDarkTheme/> }
 						lightTheme={ <WaveIconLightTheme/> }
-					/>
+					/>*/}
 
-					{/*<img src="./SVG/WaveIcon/WaveIconDarkTheme.svg" alt=""/>*/}
+					{
+						(this.context === 'dark') ?
+						<img src={ waveImageDarkTheme } alt="Wave Icon Dark Theme"/> :
+						<img src={ waveImageLightTheme } alt="WaveIconLightTheme"/>
+					}
 				</div>
-				<div className={ styles.footerInfo }>
-					<div className={ styles.description }>
-						<Paragraph fontSize='1.5rem'>
-							Wavecity is developed and maintained by Gabriel Bourget. 
-							While this application serves primarily as a web development
-							portfolio project at the moment, I'm hoping to add increasing
-							functionality to it as time goes on.
-						</Paragraph>
-					</div>
-					<div className={ styles.links }>
-						<a
-							href='https://www.github.com/gabrielbourget/WaveMusicCommunity'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							<OutlineButton
-								text='Github Repository'
-								shape='rounded'
-								onClick={ () => {} }
-							/>
-						</a>
+				<div className={ styles.description }>
+					<Paragraph fontSize='1.5rem'>
+						Wavecity is developed and maintained by Gabriel Bourget. 
+						While this application serves primarily as a web development
+						portfolio project at the moment, I'm hoping to add increasing
+						functionality to it as time goes on.
+					</Paragraph>
+				</div>
+				<div className={ styles.links }>
+					<a
+						href='https://www.github.com/gabrielbourget/WaveMusicCommunity'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
 						<OutlineButton
-							text='Home'
+							text='Github Repository'
 							shape='rounded'
 							onClick={ () => {} }
-						/>	
-						<OutlineButton
-							text='About'
-							shape='rounded'
-							onClick={ () => {} }
-						/>	
-						<OutlineButton
-							text='Documentation'
-							shape='rounded'
-							onClick={ () => {} }
-						/>							
-					</div>
+						/>
+					</a>
+					<OutlineButton
+						text='Home'
+						shape='rounded'
+						onClick={ () => {} }
+					/>	
+					<OutlineButton
+						text='About'
+						shape='rounded'
+						onClick={ () => {} }
+					/>	
+					<OutlineButton
+						text='Documentation'
+						shape='rounded'
+						onClick={ () => {} }
+					/>							
 				</div>
 			</div>
 		);
